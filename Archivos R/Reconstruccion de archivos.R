099f921b6e97982d71512cb3b53f06398ce54928
kasimxo <kasimxo@gmail.com>
2025-01-14 20:56:12 +0100

    +file reconstruction

--- Archivos R/Reconstruccion de archivos.R
+++ Archivos R/Reconstruccion de archivos.R
@@ -71,7 +71,52 @@ data<-data[-Atipicos,]
 length(which(is.na(data$Sex)==TRUE))
 #No hay ningún na en sexo
 
-
-
-
 # Aquí probamos con el dataset del lusitania
+data<-read.csv(file.choose(), header = TRUE, sep=',')
+names(data)
+columnas<-c(5,6,8,9,15,16)
+data<-data[,columnas]
+dim(data)
+length(which(is.na(data$Age)))
+# 662 NA de 1961
+desconocidos<-which(is.na(data$Age)==TRUE)
+desconocidos
+
+
+#Primero escogemos los datos conocidos
+Edadconocidos<-data$Age[-desconocidos]
+
+#Total conocidos
+length(Edadconocidos)
+
+boxplot(Edadconocidos)
+summary(Edadconocidos)
+#Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#1.00   25.00   32.00   32.72   41.00   76.00 
+#Limite superior de atipico
+LS<-41+3*(32-25)
+#Busco cuanto atípicos existen
+n<-length(which(Edadconocidos>LS))
+p<-n/length(Edadconocidos)
+p
+
+#Como p está por debajo del 5% podemos eliminar dichos datos
+Atipicos<-which(Edadconocidos>LS)
+ConocidosNoatipicos<-Edadconocidos[-Atipicos]
+length(ConocidosNoatipicos)
+
+#Buscamos una posible distribución de la edad
+#Podemos probar con la normal, exponencial, la poisson
+ks.test(ConocidosNoatipicos,"pnorm",mean=mean(ConocidosNoatipicos),sd=sd(ConocidosNoatipicos))
+ks.test(ConocidosNoatipicos,"punif", min(ConocidosNoatipicos), max(ConocidosNoatipicos))
+ks.test(ConocidosNoatipicos, "ppois", mean(ConocidosNoatipicos))
+ks.test(ConocidosNoatipicos, "pexp", rate=1/mean(ConocidosNoatipicos))
+ks.test(ConocidosNoatipicos,"dnorm",mean=mean(ConocidosNoatipicos),sd=sd(ConocidosNoatipicos))
+ks.test(ConocidosNoatipicos,"rnorm",mean=mean(ConocidosNoatipicos),sd=sd(ConocidosNoatipicos))
+ks.test(ConocidosNoatipicos,"dunif", min(ConocidosNoatipicos), max(ConocidosNoatipicos))
+ks.test(ConocidosNoatipicos,"runif", min(ConocidosNoatipicos), max(ConocidosNoatipicos))
+
+muestra<-rnorm(length(desconocidos),mean(ConocidosNoatipicos),sd(ConocidosNoatipicos))
+data$Age[desconocidos]<-muestra
+data<-data[-Atipicos,]
+
