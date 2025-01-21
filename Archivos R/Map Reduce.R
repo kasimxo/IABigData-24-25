4c33f7a16e74cf01295fd99a0840ba81f6267f74
kasimxo <kasimxo@gmail.com>
2025-01-21 19:04:46 +0100

    Improved Map Reduce file

--- Archivos R/Map Reduce.R
+++ Archivos R/Map Reduce.R
@@ -11,18 +11,70 @@ Map<-function(C){
   i<-1
   while(i<=dim(data)[[1]]){
     if(data$Survived[[i]]==C){
-      x<-append(x,data$Fare[[i]])
+      x<-append(x, data$Fare[[i]])
     }
     i<-i+1
   }
   return(x)
 }
 
-PreciosN<-Map(0)
-PreciosN
+PrecioN<-Map(0)
+PrecioS<-Map(1)
+PrecioN
+PrecioS
+
 
 # Definición de la rutina reduce
-Reduce<-function(C) {
-  
+Reduce<-function(x) {
+  y<-mean(x)
 }
 
+# Creación data frame
+PmedioS<-Reduce(PrecioS)
+PmedioN<-Reduce(PrecioN)
+
+framePreciosPorPrecio<-data.frame(PrecioS=PmedioS,PrecioN=PmediosN)
+framePreciosPorPrecio
+
+# Mi segundo MapReduce
+# Objetivo:
+# Obtener un vector con valores 1 y clave el sexo y la supervivencia:
+# Map<(Sexo, Supervivencia), Int> -> (Mujer, Si) 1
+# Rutina reduce
+# Contabilizar la frecuencia de cada salida de la rutina map por clave
+# Configurar un data frame 2x2 donde indeique la frecuencia relativa 
+# o probabilidad por supervivencia y sexo
+
+MapSexoSupervivencia<-function (sexo, sobrevive){
+  x<-c()
+  i<-1
+  while(i<=dim(data)[[1]]){
+    if(data$Survived[[i]]==sobrevive &&
+       data$Sex[[i]]==sexo){
+      x<-append(x, 1)
+    }
+    i<-i+1
+  }
+  return(x)
+}
+head(data)
+MujerS<-MapSexoSupervivencia('female',1)
+MujerN<-MapSexoSupervivencia('female',0)
+HombreS<-MapSexoSupervivencia('male',1)
+HombreN<-MapSexoSupervivencia('male',0)
+
+# Funcion reduce
+ReduceSexoSupervivencia<-function(x){
+  y<-sum(x)
+  return (y)
+}
+
+CountMujerS<-ReduceSexoSupervivencia(MujerS)
+CountMujerN<-ReduceSexoSupervivencia(MujerN)
+CountHombreS<-ReduceSexoSupervivencia(HombreS)
+CountHombreN<-ReduceSexoSupervivencia(HombreN)
+
+frameSexoSupervivencia<-data.frame(Si=c(CountMujerS,CountHombreS),
+                                   No=c(CountMujerN,CountHombreN),
+                                   row.names = c("Mujer","Hombre"))
+frameSexoSupervivencia
